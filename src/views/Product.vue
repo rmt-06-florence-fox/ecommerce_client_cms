fe995b956c270bda7ac0b82e340e4795aca6e71a
efrizal-m <efrizalsy0424@gmail.com>
2020-12-09 04:08:47 +0700

    first commit

---
+++ src/views/Product.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="product">
+    <h1>ini halaman product</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-8"></div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from '../config/axiosinstance.js'
+
+export default {
+  name: 'Product'
+}
+</script>
+
+<style>
+
+</style>
