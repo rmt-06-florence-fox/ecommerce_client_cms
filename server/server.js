57569c6f117422bcf14030d5cf37a0224bbc1dd6
YuniMarlina <yunimmarlina@gmail.com>
2020-12-11 00:20:35 +0700

    gabagus

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app')
-const http = require('http')
-const port = 3000
-
-const server = http.createServer(app)
-server.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
