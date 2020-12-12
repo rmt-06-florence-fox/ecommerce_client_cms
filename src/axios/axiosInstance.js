3e1b77eb433e2e099e717be091a8be40080936b4
Hisyam-Khowarik <hisyamkhowarik@gmail.com>
2020-12-12 21:07:57 +0700

    deploy

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
-  // baseURL: 'https://ecommerce-cms-hisyam.herokuapp.com'
+  // baseURL: 'http://localhost:3000'
+  baseURL: 'https://ecommerce-cms-16.herokuapp.com'
 })
 
 export default instance
