c5ab3091674505271cbed64cc2cf0b153d05f452
Arrasyidff <aff.anton20@gmail.com>
2020-12-12 18:45:03 +0800

    update config

--- client/src/config/axiosIntance.js
+++ client/src/config/axiosIntance.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
-// const instance = axios.create({
-//   baseURL: 'http://localhost:3000'
-// })
-
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL: 'https://cms-server-arfafa.herokuapp.com/'
 })
 
 export default instance
