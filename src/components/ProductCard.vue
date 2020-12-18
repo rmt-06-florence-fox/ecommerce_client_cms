291faf6f074edec719351091af66813f3a3b7adf
muchsin28 <hab@muchsin.id>
2020-12-18 10:32:33 +0700

    last

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -4,40 +4,52 @@
       <b-col class="col-5 d-flex align-items-center">
         <b-img
           rounded
-           height="100px"
-            width="100px"
-          :src=product.image_url
+          height="100px"
+          width="100px"
+          :src="product.image_url"
           alt="Image 1"
         ></b-img>
-        <h6 class="pl-3 text-left">{{product.name}}</h6>
+        <h6 class="pl-3 text-left">{{ product.name }}</h6>
       </b-col>
       <b-col class="col-7 d-flex align-center">
         <b-form inline class="justify-content-around">
           <b-input-group size="sm" class="col-4 m-0 p-0" prepend="IDR">
             <b-form-input
+              class="text-right"
               rounded
               size="sm"
               v-model="updateData.price"
               :placeholder="formatPrice(product.price)"
-
             ></b-form-input>
           </b-input-group>
           <b-input-group size="sm" class="col-3 m-0 p-0" append="QTY">
             <b-form-input
+              class="text-right"
               rounded
               size="sm"
-              v-model='updateData.stock'
+              v-model="updateData.stock"
               :placeholder="product.stock.toString()"
-
             ></b-form-input>
           </b-input-group>
 
-          <mdb-btn size="sm"  class="col-2 pr-1 pl-1 m-0" color="indigo" @click='updateProduct'>Update</mdb-btn>
-          <b-icon type="button" icon="trash-fill" aria-hidden="true" @click.prevent='deleteProduct'></b-icon>
+          <mdb-btn
+            size="sm"
+            class="col-2 pr-1 pl-1 m-0"
+            color="info init"
+            @click="updateProduct"
+            >Update</mdb-btn
+          >
+          <b-icon
+            type="button"
+            icon="trash-fill"
+            aria-hidden="true"
+            @click.prevent="deleteProduct"
+          ></b-icon>
         </b-form>
       </b-col>
     </b-row>
   </b-card>
+  
 </template>
 
 <script>
@@ -51,7 +63,7 @@ export default {
         id: this.product.id,
         name: this.product.name,
         image_url: this.product.image_url,
-        price: '' || this.product.price,
+        price: '' || this.formatPrice(this.product.price),
         stock: '' || this.product.stock
       }
     }
@@ -63,16 +75,16 @@ export default {
     },
     updateProduct () {
       this.$store.dispatch('updateProduct', this.updateData)
+      this.updateData.price = ''
+      this.updateData.stock = ''
     },
     deleteProduct () {
       this.$store.dispatch('deleteProduct', this.product.id)
     }
   },
   components: {
-
     mdbBtn
   }
-
 }
 </script>
 
